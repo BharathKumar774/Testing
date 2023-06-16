a86656fa1e334839f24f48e725cc990e715bd391
BharathKumar774 <107410302+BharathKumar774@users.noreply.github.com>
2023-06-16 11:20:12 +0530

    Add files via upload
    huihy

---
+++ Basic.java
@@ -0,0 +1,21 @@
+package Intro;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        DesiredCapabilities cp = new DesiredCapabilities();
+        cp.setCapability(ChromeOptions.CAPABILITY, options);
+        options.merge(cp);
+        WebDriver driver = new ChromeDriver(options);
+
+	}
+
+}

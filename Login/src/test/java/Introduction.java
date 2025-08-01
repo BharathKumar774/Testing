1463b295096eaa5625dd0c5b89bca42bd3fed83a
BharathKumar774 <107410302+BharathKumar774@users.noreply.github.com>
2025-08-01 14:19:42 +0530

    Delete Login directory

--- Login/src/test/java/Introduction.java
+++
@@ -1,8 +0,0 @@
-
-public class Introduction {
- public static void main(String[] args) {
-	 //Step 1 invoking the browser by adding a ChromeDriver class-Methods
-	 //need to creat an object of the class to use the methods
-	 ChromeDriver driver = new ChromeDriver();
- }
-}
